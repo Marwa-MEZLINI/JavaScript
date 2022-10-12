b4c7aa88ebc97390f35a09dd7d30f1c22a612276
Marwa-MEZLINI <m.mezlini92@gmail.com>
2022-10-12 14:17:43 +0100

    exercice 2 done

---
+++ exercices/Exercice2/java.js
@@ -0,0 +1,79 @@
+//(1)
+let challenge = 'Zone4Code JavaScript'
+
+//(2)
+console.log(challenge)
+
+//(3)
+console.log(challenge.length)
+
+//(4)
+console.log(challenge.toUpperCase())
+
+//(5)
+console.log(challenge.toLocaleLowerCase())
+
+//(6)
+console.log(challenge.substring(0, 4))
+
+//(7)
+console.log(challenge.substring(0, 9))
+
+//(8)
+console.log(challenge.includes('Script'))
+
+//(9)
+console.log(challenge.split("|"))
+
+//(10)
+console.log(challenge.split(" "))
+
+//(11)
+let string = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(string.split(","))
+
+//(12)
+console.log(challenge.replace("JavaScript", "Python"))
+
+//(13)
+console.log(challenge.charAt(5))
+
+//(14)
+console.log(challenge.charCodeAt("J"))
+
+//(15)
+console.log(challenge.indexOf('a'))
+
+//(16)
+console.log(challenge.lastIndexOf('a'))
+
+//(17)
+let text = 'You cannot end a sentence with because because because is a conjunction'
+console.log(text.indexOf('because'))
+
+//(18)
+console.log(text.lastIndexOf('because'))
+
+//(19)
+console.log(text.search('because'))
+
+//(20)
+let _string =  '     Zone4Code JavaScript   '
+console.log(_string)
+console.log(_string.trim())
+
+//(21)
+console.log(challenge.startsWith('Z'))
+
+//(22)
+console.log(challenge.endsWith('t'))
+
+//(23)
+console.log(challenge.match("a"))
+
+//(24)
+let string1 = "Zone4Code", string2 = "JavaScript"
+console.log(string1.concat(" ",string2))
+
+//(25)
+console.log(challenge.repeat(2))
