058f8ed171bb70c0f1c1b06012cc9b7f29ce5f29
Marwa-MEZLINI <m.mezlini92@gmail.com>
2022-10-19 11:40:30 +0100

    level 3 : question 2 in progress

--- Higher_Order_Function_/Exercices_Level_3/level3.js
+++ Higher_Order_Function_/Exercices_Level_3/level3.js
@@ -36,3 +36,23 @@ const sortByPopulation = countries.sort((a,b)=> {
 })
 
 console.log(sortByPopulation);
+
+//(2)
+
+    const laguageCount = []
+  countries.forEach((item) => {
+    laguageCount [item.languages] = (laguageCount[item.languages] || 0) +1 ;
+  });
+
+  const sortLaguageCount = laguageCount.sort((a,b)=> {
+    if (a.population > b.population){
+        return 1
+    }
+    else
+    {
+        return -1
+    }
+})
+
+  console.log(laguageCount);
+  
