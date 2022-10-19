54582c0485ca33bfee9ad87fbdcfa9dbc6489f29
Marwa-MEZLINI <m.mezlini92@gmail.com>
2022-10-19 10:40:52 +0100

    level 2 : question 7 (level done)

--- Higher_Order_Function_/Exercices_Level_2/level2.js
+++ Higher_Order_Function_/Exercices_Level_2/level2.js
@@ -1,12 +1,12 @@
 import { countries } from "./countries.js"
 //(1)
 const products = [
-    { Product : 'banana', price : 3},
-    { Product : 'mango', price : 6},
-    { Product : 'potato', price : '' },
-    { Product : 'avocado', price : 8},
-    { Product : 'coffee', price : 10},
-    { Product : 'tea', price : '' },
+  { Product: 'banana', price: 3 },
+  { Product: 'mango', price: 6 },
+  { Product: 'potato', price: '' },
+  { Product: 'avocado', price: 8 },
+  { Product: 'coffee', price: 10 },
+  { Product: 'tea', price: '' },
 ]
 const totalPrice = products.filter((item) => typeof item.price === "number").reduce((item, index) => item + index.price, 0);
 
@@ -14,29 +14,28 @@ console.log(totalPrice);
 
 //(2)
 
-const sumOfPrice = products.reduce(function(accumulator, object)
- {
+const sumOfPrice = products.reduce(function (accumulator, object) {
 
-    return accumulator + ((object.price) !== '' && object.price);
-  }, 0);
-  
-  console.log(sumOfPrice); 
+  return accumulator + ((object.price) !== '' && object.price);
+}, 0);
+
+console.log(sumOfPrice);
 
 //(3)
 function categorizeCountries(keyword) {
-    console.log(countries.filter((country) => country.name.includes(keyword)))
+  console.log(countries.filter((country) => country.name.includes(keyword)))
 }
 categorizeCountries('stan')
 
 //(4)
-function letterNumber(){
+function letterNumber() {
   const alphaChar = 'ABCDEFGHJKLMNOPQRSTUVWXYZ'
   const lettercount = []
-  
 
-  for (let i = 0 ; i < (alphaChar.length) ; i++){
+
+  for (let i = 0; i < (alphaChar.length); i++) {
     const letter = countries.filter((item) => item.name.startsWith(alphaChar[i]))
-    lettercount.push({'letter' : alphaChar[i] , 'number' : letter.length + 'times'})
+    lettercount.push({ 'letter': alphaChar[i], 'number': letter.length })
   }
 
   return (lettercount)
@@ -45,15 +44,21 @@ function letterNumber(){
 console.log(letterNumber())
 
 //(5)
-function getFirstCountries(){
-return countries.slice(0,10)
+function getFirstCountries() {
+  return countries.slice(0, 10)
 }
 
 console.log(getFirstCountries())
 
 //(6)
-function getLastCountries(){
+function getLastCountries() {
   return countries.slice((countries.length - 10))
 }
 
 console.log(getLastCountries());
+
+//(7)
+const res = letterNumber()
+const mostUsedLetter = res.reduce((prev, current) => (prev.number > current.number) ? prev : current)
+
+console.log(mostUsedLetter);
