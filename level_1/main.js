7f2fdd29a379e7dfa4d310885a39c231294ef1a0
Marwa-MEZLINI <m.mezlini92@gmail.com>
2022-10-24 11:46:20 +0100

    level 2 done

--- level_1/main.js
+++ level_1/main.js
@@ -43,3 +43,46 @@ allParag[2].id = "new-id"
 console.log(allParag[2]);
 allParag[3].classList.add('parag','fourth')
 console.log(allParag[3]);
+
+//-----------------level 2 ---------------------------//
+//(1)
+allParag[0].style.fontFamily = "calibri"
+allParag[1].style.textAlign = 'center'
+allParag[2].style.padding = '16px'
+allParag[3].style.margin = "15px"
+
+//(2)
+for ( let i =0 ; i< allParag.length ; i ++){
+  allParag[i].style.fontSize = '24px' 
+  allParag[i].style.padding = '15px'
+  allParag[i].style.margin = '5px'
+  allParag[i].style.borderRadius = "30px"
+  allParag[i].style.textAlign = 'center'
+  allParag[i].style.fontWeight = 'bolder'
+  if (i % 2 === 0) {
+    allParag[i].style.color = 'green'
+    allParag[i].style.backgroundColor = 'yellow'
+  } else {
+    allParag[i].style.color = 'red'
+    allParag[i].style.backgroundColor = 'orange'
+  }
+}
+
+//(3)
+allParag[0].textContent = "This Is The First Paragraph"
+allParag[1].textContent = "This Is The Second Paragraph"
+allParag[2].textContent = "This Is The Third Paragraph"
+allParag[3].textContent = "This Is The Fourth Paragraph"
+
+allParag[0].className= 'para1'
+allParag[1].className= 'para2'
+allParag[2].className= 'para3'
+allParag[3].className= 'para4'
+
+allParag[0].id = 'first'
+allParag[1].id = 'second'
+allParag[2].id = 'third'
+allParag[3].id = 'fourth'
+
+
+
